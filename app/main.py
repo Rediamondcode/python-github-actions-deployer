d71fcb0809d5faa17c0861c0ddcbc0960d286120
akanji yusuf ayinla <104696367+Rediamondcode@users.noreply.github.com>
2026-01-23 17:17:17 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+import socket
+import datetime
 
 def create_app():
     app = Flask(__name__)
 
+    APP_VERSION = "1.0.1"  # change this to test CI/CD
+    DEPLOYED_AT = datetime.datetime.utcnow().isoformat() + "Z"
+    HOSTNAME = socket.gethostname()
+
     @app.route("/")
     def home():
-        return "🚀 Python App Deployed with GitHub Actions CI/CD!"
+        return f"""
+        🚀 Python App Deployed with GitHub Actions CI/CD!<br><br>
+        <b>Version:</b> {APP_VERSION}<br>
+        <b>Deployed at (UTC):</b> {DEPLOYED_AT}<br>
+        <b>Server:</b> {HOSTNAME}
+        """
 
     @app.route("/health")
     def health():
-        return {"status": "ok"}
+        return jsonify(
+            status="ok",
+            version=APP_VERSION,
+            server=HOSTNAME
+        )
 
     return app
